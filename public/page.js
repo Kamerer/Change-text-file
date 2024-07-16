1a1fef684475a7e630fa20355fcc3b60cce7a5ee
Max <makasya.drofa@gmail.com>
2024-07-16 23:16:48 +0700

    Added text field

--- public/page.js
+++ public/page.js
@@ -1,22 +1,49 @@
+document.getElementById('textInput').addEventListener('keydown', handleTextSelect);
 document.getElementById('fileInput').addEventListener('change', handleFileSelect);
+
 document.getElementById('uploadButton').addEventListener('click', uploadFile);
 document.getElementById('modifyButton').addEventListener('click', modifyFile);
 
 let selectedFile = null;
+let selectedText = '';
 const formData = new FormData();
 
+function handleTextSelect(event) {
+    if (event.key === "Enter") {
+        document.getElementById('uploadButton').disabled = false;
+        selectedText = document.getElementById('textInput').value;
+        console.log(selectedText);
+    }
+}
 function handleFileSelect(event) {
     selectedFile = event.target.files[0];
     document.getElementById('uploadButton').disabled = !selectedFile;
-    document.getElementById('modifyButton').disabled = !selectedFile;
+
+    if (selectedFile) {
+        // Создаем FileReader для чтения файла
+        const reader = new FileReader();
+
+        // Определяем, что делать, когда файл будет прочитан
+        reader.onload = function(e) {
+            selectedText = e.target.result;
+            console.log('Содержимое файла:', selectedText);
+
+            // Выводим содержимое файла в элемент с id "output"
+            document.getElementById('rawText').textContent = selectedText;
+        };
+        // Читаем файл как текст
+        reader.readAsText(selectedFile);
+    } else {
+        console.log('Файл не выбран');
+    }
 }
 
 function uploadFile() {
     if (!selectedFile) {
         return;
     }
+    document.getElementById('modifyButton').disabled = !selectedFile;
     formData.append('file', selectedFile);
-    document.getElementById('rawText').textContent = formData;
 }
 function modifyFile() {
     fetch('/upload', {
