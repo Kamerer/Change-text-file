bc795e065a899e760011051fc0956f15021fee2d
Max <makasya.drofa@gmail.com>
2024-07-19 22:09:13 +0700

    Playng with styles

--- public/frontJS/copyToBuffer.js
+++ public/frontJS/copyToBuffer.js
@@ -1,7 +1,6 @@
 function copyText() {
-    // Получаем элемент textarea
     var textArea = document.getElementById("resultText");
-    console.log('fffff');
+
     // Копируем текст в буфер обмена с использованием Clipboard API
     navigator.clipboard.writeText(textArea.value).then(function() {
         console.log("Текст скопирован: " + textArea.value);
